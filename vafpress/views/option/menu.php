972bf8c1f1e64ebe1eccfa5cc43faa80d418e0de
David Rozando <david.rozando@gmail.com>
2013-03-20 15:49:42 +0700

    Panel Title

--- vafpress/views/option/menu.php
+++ vafpress/views/option/menu.php
@@ -6,6 +6,7 @@
 	<?php endforeach; ?>
 <?php else: ?>
 <div id="<?php echo $menu->get_name(); ?>" class="vp-panel<?php if((isset($sub_current) and $sub_current) or (!isset($sub_current) and $current)) echo ' vp-current'; ?>">
+	<h2><span><?php echo $menu->get_title(); ?></span></h2>
 	<?php foreach ($menu->get_controls() as $control): ?>
 	<?php echo $control->render(); ?>
 	<?php endforeach; ?>
