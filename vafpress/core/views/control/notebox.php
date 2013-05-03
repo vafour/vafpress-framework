cc361e504695163c588fe061320b6ece8e3ffa11
Ayub <ayublin@gmail.com>
2013-05-03 16:16:32 +0700

    notebox is compact

--- vafpress/core/views/control/notebox.php
+++ vafpress/core/views/control/notebox.php
@@ -1,9 +1,12 @@
 <?php extract($head_info); ?>
+
+<?php if(!$is_compact): ?>
 <div class="vp-field <?php echo $type; ?><?php echo !empty($container_extra_classes) ? (' ' . $container_extra_classes) : ''; ?>"
 	data-vp-type="<?php echo $type; ?>"
 	<?php echo VP_Util_Text::print_if_exists(isset($dependency) ? $dependency : '', 'data-vp-dependency="%s"'); ?>
 	<?php echo $is_hidden ? 'style="display: none;"' : ''; ?>
 	id="<?php echo $name; ?>">
+<?php endif; ?>
 	<?php switch ($status) {
 		case 'normal':
 			$icon_class = 'icon-lightbulb';
@@ -27,4 +30,7 @@
 	<i class="<?php echo $icon_class; ?>"></i>
 	<div class="label"><?php echo $label; ?></div>
 	<?php VP_Util_Text::print_if_exists($description, '<div class="description">%s</div>'); ?>
+
+<?php if(!$is_compact): ?>
 </div>
+<?php endif; ?>
