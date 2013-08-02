990307f05af4d3959a8fab55dbb9698be5470834
Ayub <ayublin@gmail.com>
2013-08-02 18:18:20 +0700

    New Vafpress 2.0

--- vafpress/core/views/control/template_control_foot.php
+++
@@ -1,5 +0,0 @@
-		</div>
-		<div class="vp-js-bind-loader vp-field-loader vp-hide"><img src="<?php VP_Util_Res::img_out('ajax-loader.gif', ''); ?>" /></div>
-		<div class="validation-msgs"><ul></ul></div>
-	</div>
-</div>
