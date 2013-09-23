dd25aa8802d74ca63f0d528114caf2faef761a0f
Ayub <ayublin@gmail.com>
2013-09-23 12:13:24 +0700

    use untrailingslashit to be consistent and make sure before concatenating

--- constant.php
+++ constant.php
@@ -8,34 +8,29 @@
 
 defined('VP_VERSION')     or define('VP_VERSION'    , '2.0-beta');
 defined('VP_NAMESPACE')   or define('VP_NAMESPACE'  , 'VP_');
-defined('VP_DIR')         or define('VP_DIR'        , dirname(__FILE__));
+defined('VP_DIR')         or define('VP_DIR'        , untrailingslashit(dirname(__FILE__)));
 defined('VP_DIR_NAME')    or define('VP_DIR_NAME'   , basename(VP_DIR));
-defined('VP_IMAGE_DIR')   or define('VP_IMAGE_DIR'  , trailingslashit(VP_DIR) . 'public/img');
-defined('VP_CONFIG_DIR')  or define('VP_CONFIG_DIR' , trailingslashit(VP_DIR) . 'config');
-defined('VP_DATA_DIR')    or define('VP_DATA_DIR'   , trailingslashit(VP_DIR) . 'data');
-defined('VP_CLASSES_DIR') or define('VP_CLASSES_DIR', trailingslashit(VP_DIR) . 'classes');
-defined('VP_VIEWS_DIR')   or define('VP_VIEWS_DIR'  , trailingslashit(VP_DIR) . 'views');
-defined('VP_INCLUDE_DIR') or define('VP_INCLUDE_DIR', trailingslashit(VP_DIR) . 'includes');
+defined('VP_IMAGE_DIR')   or define('VP_IMAGE_DIR'  , VP_DIR . '/public/img');
+defined('VP_CONFIG_DIR')  or define('VP_CONFIG_DIR' , VP_DIR . '/config');
+defined('VP_DATA_DIR')    or define('VP_DATA_DIR'   , VP_DIR . '/data');
+defined('VP_CLASSES_DIR') or define('VP_CLASSES_DIR', VP_DIR . '/classes');
+defined('VP_VIEWS_DIR')   or define('VP_VIEWS_DIR'  , VP_DIR . '/views');
+defined('VP_INCLUDE_DIR') or define('VP_INCLUDE_DIR', VP_DIR . '/includes');
 
 // detect url whether it's theme or plugin integrated
 $dirname = str_replace('\\' ,'/', dirname(__FILE__));
 $dirname = preg_replace('|/+|', '/', $dirname);
 $dir_url = plugin_dir_url(__FILE__);
-if( strpos($dir_url, $dirname) === false )
-{
-	$base_url = $dir_url;
-	$vp_url   = $base_url;
-}
-else
-{
-	$base_url = get_template_directory_uri();
-	$vp_url   = trailingslashit($base_url) . VP_DIR_NAME;
-}
 
-defined('VP_URL')         or define('VP_URL'        , $vp_url);
-defined('VP_PUBLIC_URL')  or define('VP_PUBLIC_URL' , trailingslashit(VP_URL)        . 'public');
-defined('VP_IMAGE_URL')   or define('VP_IMAGE_URL'  , trailingslashit(VP_PUBLIC_URL) . 'img');
-defined('VP_INCLUDE_URL') or define('VP_INCLUDE_URL', trailingslashit(VP_URL)        . 'includes');
+if( strpos($dir_url, $dirname) === false )
+	$vp_url   = $dir_url;
+else
+	$vp_url   = trailingslashit(get_template_directory_uri()) . VP_DIR_NAME;
+
+defined('VP_URL')         or define('VP_URL'        , untrailingslashit($vp_url));
+defined('VP_PUBLIC_URL')  or define('VP_PUBLIC_URL' , VP_URL        . '/public');
+defined('VP_IMAGE_URL')   or define('VP_IMAGE_URL'  , VP_PUBLIC_URL . '/img');
+defined('VP_INCLUDE_URL') or define('VP_INCLUDE_URL', VP_URL        . '/includes');
 
 // Get the start time and memory usage for profiling
 defined('VP_START_TIME')  or define('VP_START_TIME', microtime(true));
