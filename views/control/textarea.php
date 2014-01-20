3cdd26ea6e5118a1cfc1048188382a368c06f444
Ayub <ayublin@gmail.com>
2014-01-20 13:17:49 +0700

    escape value

--- views/control/textarea.php
+++ views/control/textarea.php
@@ -1,5 +1,5 @@
 <?php if(!$is_compact) echo VP_View::instance()->load('control/template_control_head', $head_info); ?>
 
-<textarea class="vp-input" name="<?php echo $name; ?>"><?php echo $value; ?></textarea>
+<textarea class="vp-input" name="<?php echo $name; ?>"><?php echo esc_attr($value); ?></textarea>
 
 <?php if(!$is_compact) echo VP_View::instance()->load('control/template_control_foot'); ?>
